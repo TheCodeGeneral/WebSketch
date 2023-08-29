123498d868d44ae4066a83fae0ea7081d9ed64c4
Cameron Pierce <camrpierce@gmail.com>
2023-08-29 14:04:49 -0500

    Changed grid generation

--- javascript.js
+++ javascript.js
@@ -1,18 +1,24 @@
-console.log('loaded');
-generateGrid(16,16);
-function generateGrid(numRows, numCols) {
+generateGrid(16);
+
+function generateGrid(numRows) {
+    // Create div to put pixels in
     grid = document.createElement('div');
     grid.id = 'grid';
-    for(i = 0; i < numRows; i++) {
-        row = document.createElement('div');
-            row.className = 'grid-row';
-        for(j = 0; j < numCols; j++) {
-            col = document.createElement('div');
-            col.className = 'grid-column';
-            row.appendChild(col);
-        }
-        grid.appendChild(row);
-    }
 
-    document.querySelector('body').appendChild(grid);
+    // Get css custom property for grid size and border width
+    let gridSize = parseInt(getComputedStyle(document.documentElement,null).getPropertyValue('--grid-size').slice(0,-2));
+
+    for(i = 0; i < numRows * numRows; i++) {
+        // Create pixel
+        pixel = document.createElement('div');
+        pixel.className = 'pixel';
+
+        // Calculate and set pixel size based on grid size
+        let pixelSize = gridSize / numRows;
+        pixel.setAttribute('style', `width: ${pixelSize}px; height: ${pixelSize}px;`);
+        // Add pixel to grid
+        grid.appendChild(pixel);
+    }
+    
+    document.getElementById('grid-wrapper').appendChild(grid);
 }
