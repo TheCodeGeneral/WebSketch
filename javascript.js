dda12d7b7cebd57f647590cf7984a1d279d51362
Cameron Pierce <camrpierce@gmail.com>
2023-08-30 05:01:47 -0500

    Added drawing colors in grid

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
 generateGrid(16);
 
+
 function generateGrid(numRows) {
     // Create div to put pixels in
     grid = document.createElement('div');
     grid.id = 'grid';
 
-    // Get css custom property for grid size and border width
+    // Get css custom property for grid size
     let gridSize = parseInt(getComputedStyle(document.documentElement,null).getPropertyValue('--grid-size').slice(0,-2));
 
     for(i = 0; i < numRows * numRows; i++) {
@@ -19,6 +20,16 @@ function generateGrid(numRows) {
         // Add pixel to grid
         grid.appendChild(pixel);
     }
-    
+
     document.getElementById('grid-wrapper').appendChild(grid);
 }
+
+function drawColor(pixel) {
+    if(pixel.which) {
+        let color = document.getElementById('color-picker').value;
+        pixel.target.style.backgroundColor = color;
+    }
+}
+document.querySelectorAll('#grid > div').forEach(pixel => {
+    pixel.addEventListener('mouseenter', drawColor);
+});
