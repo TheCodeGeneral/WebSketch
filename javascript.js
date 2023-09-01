2b731c0d4d589f8e145ab6abc948492d3c7a4810
TheCodeGeneral <camrpierce@gmail.com>
2023-09-01 05:22:29 -0500

    Added color presets

--- javascript.js
+++ javascript.js
@@ -30,6 +30,30 @@ function drawColor(pixel) {
         pixel.target.style.backgroundColor = color;
     }
 }
+
+function changeColor(color) {
+    // Get color from div and convert to hexcode
+    let newColor = rgba2hex(getComputedStyle(color.target).backgroundColor);
+    document.getElementById('color-picker').value = newColor;
+}
+
+// Source https://stackoverflow.com/questions/1740700/how-to-get-hex-color-value-rather-than-rgb-value
+const rgba2hex = (rgba) =>
+  `#${rgba
+    .match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/)
+    .slice(1)
+    .map((n, i) =>
+      (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n))
+        .toString(16)
+        .padStart(2, "0")
+        .replace("NaN", ""),
+    ).join("")}`;
+
 document.querySelectorAll('#grid > div').forEach(pixel => {
     pixel.addEventListener('mouseenter', drawColor);
 });
+
+document.querySelectorAll('.color').forEach(color => {
+    color.addEventListener('click', changeColor);
+});
+
